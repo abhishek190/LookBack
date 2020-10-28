899c6bd5ee4100a56d72a1c37dc0fc8b5b7a961a
Abhishek Bachchan <49363158+abhishek190@users.noreply.github.com>
2020-10-28 12:33:12 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 var bcrypt=require('./bcrypt.js-master/bcrypt.js-master/dist/bcrypt.js');
-var url="mongodb+srv://abhishek190:Jamesss@007@cluster0.iidxh.mongodb.net/mydb?retryWrites=true&w=majority"; 
+var url=process.env.MONGODB_URI; 
 mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true});
 var db=mongoose.connection;
 var userSchema=new mongoose.Schema({
